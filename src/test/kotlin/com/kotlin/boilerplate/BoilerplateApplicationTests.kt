1c60ea4dc167beb2f2b4ca935d47bffe8b4ba8c3
jspark <98972385+JunseoParKK@users.noreply.github.com>
2024-11-13 05:33:11 +0900

    chore: initial project settings

---
+++ src/test/kotlin/com/kotlin/boilerplate/BoilerplateApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.kotlin.boilerplate
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BoilerplateApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
