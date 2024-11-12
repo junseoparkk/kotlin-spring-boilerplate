0f28b14b852ee53526c4929c1aeeea3b2140a688
jspark <98972385+JunseoParKK@users.noreply.github.com>
2024-11-13 06:31:18 +0900

    feat: add @ConfigurationPropertiesScan to project root

--- src/main/kotlin/com/kotlin/boilerplate/BoilerplateApplication.kt
+++ src/main/kotlin/com/kotlin/boilerplate/BoilerplateApplication.kt
@@ -1,9 +1,11 @@
 package com.kotlin.boilerplate
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 class BoilerplateApplication
 
 fun main(args: Array<String>) {
